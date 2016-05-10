4ffe66e522354dbd34108f9371857bbb04ea396e
Paul Chiusano <paul.chiusano@gmail.com>
2016-05-10 10:16:05 -0400

    New pure implementation of join (not working)

--- core/src/main/scala/fs2/concurrent.scala
+++ core/src/main/scala/fs2/concurrent.scala
@@ -1,5 +1,7 @@
 package fs2
 
+import fs2.async.mutable.Queue
+import Stream.Handle
 
 object concurrent {
 
@@ -24,45 +26,45 @@ object concurrent {
     *                   all other `inner` streams are interrupted as well, resulting in stream that fails
     *                   with error of the stream that cased initial failure.
     */
-  // todo: document when the finalizers are calle in which situation
-  def join[F[_],O](maxOpen: Int)(outer: Stream[F,Stream[F,O]])(implicit F: Async[F]): Stream[F,O] = {
-    assert(maxOpen > 0,"maxOpen must be > 0, was: " + maxOpen)
-    for {
-      killSignal <- Stream.eval(async.signalOf(false))
-      openLeases <- Stream.eval(async.mutable.Semaphore(maxOpen))
-      outerDone <- Stream.eval(F.refOf[Boolean](false))
-      outputQueue <- Stream.eval(async.mutable.Queue.synchronousNoneTerminated[F,Either[Throwable,Chunk[O]]])
-      o <- outer.evalMap { (inner: Stream[F,O]) =>
-        F.bind(openLeases.decrement) { _ =>
-        F.start {
-          val done =
-            F.bind(openLeases.increment) { _ =>
-            F.bind(F.get(outerDone)) { outerDone =>
-            F.bind(openLeases.count) { n =>
-              if (n == maxOpen && outerDone) {
-                outputQueue.enqueue1(None)
-              }
-              else F.pure(())
-            }}}
-          inner.chunks.attempt.evalMap { o => outputQueue.enqueue1(Some(o)) }
-          .interruptWhen(killSignal)
-          .onFinalize(done)
-          .run.run
-        }}
-      }.onFinalize(F.bind(openLeases.count) { n =>
-          F.bind(if (n == maxOpen) outputQueue.offer1(None) else F.pure(true)) { _ =>
-            F.setPure(outerDone)(true)
-          }
-      }) mergeDrainL {
-        outputQueue.dequeue.through(pipe.unNoneTerminate).flatMap {
-          case Left(e) => Stream.eval(killSignal.set(true)).flatMap { _ => Stream.fail(e) }
-          case Right(c) => Stream.chunk(c)
-        }
-      } onFinalize {
-        // set kill signal, then wait for any outstanding inner streams to complete
-        F.bind(killSignal.set(true)) { _ =>
-        F.bind(openLeases.clear) { m => openLeases.decrementBy(maxOpen - m) }}
+  def join[F[_],O](maxOpen: Int)(s: Stream[F,Stream[F,O]])(implicit F: Async[F]): Stream[F,O] = {
+    if (maxOpen <= 0) throw new IllegalArgumentException("maxOpen must be > 0, was: " + maxOpen)
+    trait StepResult
+    case object Interrupted extends StepResult
+    case class NewInnerStream(step: Pull[F,Nothing,Step[Option[Stream[F,O]],Handle[F,Stream[F,O]]]], onForce: Scope[F,Unit])
+      extends StepResult
+    case class InnerStep(step: Pull[F,Nothing,Step[Chunk[O],Handle[F,O]]], onForce: Scope[F,Unit])
+      extends StepResult
+    def pumpOuter(q: Queue[F,StepResult])(s: Handle[F,Stream[F,O]]): Pull[F,Nothing,Unit] =
+      s.await1Async.flatMap { f =>
+        Pull.eval(F.start { F.bind(f.get) { s => q.enqueue1(NewInnerStream(s._1, s._2)) }}) as (())
       }
-    } yield o
+    def go(outerDone: Boolean, q: Queue[F,StepResult], open: Int): Pull[F,O,Unit] = {
+      def pumpInner(s: Handle[F,O]): Pull[F,Nothing,Unit] = s.awaitAsync.flatMap { f =>
+        Pull.eval(F.start { F.bind(f.get) { p => q.enqueue1(InnerStep(p._1, p._2)) }}) as (())
+      }
+      Pull.eval(q.dequeue1).flatMap { // todo - make this dequeue interruptible
+        case NewInnerStream(newS,onForce) => Pull.evalScope(onForce) >>
+          newS.flatMap {
+            case None #: _ => go(outerDone = true, q, open)
+            case Some(hd) #: tl => hd.open.flatMap(pumpInner) >> {
+              if (open < maxOpen) pumpOuter(q)(tl) >> go(outerDone, q, open + 1)
+              else go(outerDone, q, open + 1)
+            }
+          }
+        case InnerStep(winner,onForce) => Pull.evalScope(onForce) >> winner.optional.flatMap {
+          case None =>
+            if (open == 1 && outerDone) Pull.done // outer stream is done + this is last inner stream to complete
+            else go(outerDone, q, open - 1)
+          case Some(out #: h) =>
+            // winner has more values, write these to output
+            // and update the handle for the winning position
+            Pull.output(out) >> pumpInner(h) >> go(outerDone, q, open)
+        }
+        case Interrupted => Pull.done
+      }
+    }
+    Stream.eval(async.unboundedQueue[F,StepResult]).flatMap { q =>
+      s.pull { h => pumpOuter(q)(h) >> go(false, q, 0) }.onFinalize(q.enqueue1(Interrupted))
+    }
   }
 }
